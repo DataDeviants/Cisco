1034369edbad0c9e073648c287192a5dff21d38b
Luis Wirth <lwirth2000@gmail.com>
2024-03-21 02:55:30 +0100

    scale map and confidence

--- plot.py
+++ plot.py
@@ -5,23 +5,33 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("pos.csv")
 print(df.head())
 
+df = df[df['floorNumber'] == 1]
+length = 228.30688
+width = 346.92673
+height = 13.0
+offsetX = 14.67
+offsetY = 21.18
+imageWidth = 4162
+imageHeight = 2739
+
+df = df[df['confidence'] > 100]
+
 # collect data for each id
 ids = df['id'].unique()
 
 # filter out the ids that are stationary (don't move)
 ids = [id for id in ids if len(df[df['id'] == id]) > 10]
 
-plt.imshow(plt.imread('floor.png'))
+plt.imshow(plt.imread('floor1.png'))
 
-#ids = ids[200:250]
+#ids = ids[200:220]
 for id in ids:
     data = df[df['id'] == id]
-    datax = data['x'].rolling(window=30).mean()
-    datay = data['y'].rolling(window=30).mean()
+    datax = data['x'].rolling(window=1).mean()
+    datay = data['y'].rolling(window=1).mean()
     
-    # scale x and y by 10
-    datax = datax * 10
-    datay = datay * 10
+    datax = (datax + offsetX) / (width + 2 * offsetX) * imageWidth
+    datay = (datay + offsetY) / (length + 2 * offsetY) * imageHeight
     plt.plot(datax, datay, label=id)
 
 plt.savefig('plot.png')
