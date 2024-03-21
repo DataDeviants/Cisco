fbc143559170d4414642678cac1a098d2d416ebc
Valentin Vogt <vogtva@student.ethz.ch>
2024-03-21 02:14:44 +0100

    both floors

--- floor.py
+++ floor.py
@@ -4,18 +4,19 @@ import socket
 import os
 import sys
 
+
 def get_API_Key_and_auth():
-  # Gets public key from spaces and places in correct format
-  print("-- No API Key Found --")
+    # Gets public key from spaces and places in correct format
+    print("-- No API Key Found --")
 
-  # Gets user to paste in generated token from app
-  token = input('Enter provided API key here: ')
+    # Gets user to paste in generated token from app
+    token = input("Enter provided API key here: ")
 
-  # Writes activation key to file. This key can be used to open up Firehose connection
-  f = open("API_KEY.txt", "a")
-  f.write(token)
-  f.close()
-  return token
+    # Writes activation key to file. This key can be used to open up Firehose connection
+    f = open("API_KEY.txt", "a")
+    f.write(token)
+    f.close()
+    return token
 
 
 # work around to get IP address on hosts with non resolvable hostnames
@@ -23,30 +24,33 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 IP_ADRRESS = s.getsockname()[0]
 s.close()
-url = 'http://' + str(IP_ADRRESS) + '/update/'
+url = "http://" + str(IP_ADRRESS) + "/update/"
 
 # Tests to see if we already have an API Key
 try:
-  if os.stat("API_KEY.txt").st_size > 0:
-    # If we do, lets use it
-    f = open("API_KEY.txt")
-    apiKey = f.read()
-    f.close()
-  else:
-    # If not, lets get user to create one
-    apiKey = get_API_Key_and_auth()
+    if os.stat("API_KEY.txt").st_size > 0:
+        # If we do, lets use it
+        f = open("API_KEY.txt")
+        apiKey = f.read()
+        f.close()
+    else:
+        # If not, lets get user to create one
+        apiKey = get_API_Key_and_auth()
 except:
-  apiKey = get_API_Key_and_auth()
+    apiKey = get_API_Key_and_auth()
+
+map_ids = ['a06add6866a28b3ce9ca6f5de19692d9', '5e3d50992e29fe366011d57a87870de9']
 
 s = requests.Session()
-s.headers = {'X-API-Key': apiKey}
-# get the floor map via a FloorMapRequest
-r = s.get(
-    'https://partners.dnaspaces.io/api/partners/v1/maps/5e3d50992e29fe366011d57a87870de9/image')
-print(r.status_code)
-# md5 to image
-md5 = r.headers['Content-MD5']
-print(md5)
-# save the image
-with open('floor.png', 'wb') as f:
-    f.write(r.content)
+s.headers = {"X-API-Key": apiKey, "partnerTenantId": "Simulation-Workspaces"}
+
+for j, id in enumerate(map_ids):
+    r = s.get(
+        "https://partners.dnaspaces.io/api/partners/v1/maps/" + id + "/image"
+    )
+    print(r.status_code)
+    # md5 to image
+    md5 = r.headers["Content-MD5"]
+    # save the image
+    with open("floor" + str(j+1) + ".png", "wb") as f:
+        f.write(r.content)
